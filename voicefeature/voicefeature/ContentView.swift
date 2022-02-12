3d23df47afbd6e5a2d07a2d698f7027f25f7f20a
Umang Gupta <57800546+Usgupta@users.noreply.github.com>
2022-02-13 03:06:00 +0800

    added voicefeature folder

---
+++ voicefeature/voicefeature/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  voice reply
+//
+//  Created by Umang Gupta on 7/2/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
+    
+    var transcript: String?
+    @State var tts = texttospeech()
+//    var tts: texttospeech
+    
+    
+    
+    var body: some View {
+        
+        VStack {
+            Button("Verify Bus Timing", action: {
+                
+                print("what bus number are you waiting for")
+                
+                
+
+                
+                speechRecognizer.reset()
+                speechRecognizer.transcribe()
+                isRecording = true
+                
+//                print(isRecording)
+                print(speechRecognizer.transcript)
+                
+            })
+            
+            
+            Button("Stop recording", action: {
+                
+                print("stopped")
+                
+                speechRecognizer.stopTranscribing()
+                isRecording = false
+//                print(isRecording)
+                print(speechRecognizer.transcript)
+                
+            })
+            
+            Button("Speak out Custom Text", action: {
+                
+                tts.initutterance()
+                tts.speechsynthesiser()
+                
+            })
+        }
+        
+        
+        
+        
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
---
+++ voicefeature/voicefeature/voicefeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  voicefeatureApp.swift
+//  voicefeature
+//
+//  Created by Umang Gupta on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct voicefeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
