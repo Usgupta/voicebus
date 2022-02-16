aac13e46f1e54b5349f04ed229c503319325ad7f
Priscilla Pearly Tan <priscilla_tan@mymail.sutd.edu.sg>
2022-02-16 15:45:29 +0800

    UI updates
    :)

---
+++ SpeechView.swift
@@ -0,0 +1,21 @@
+//
+//  SpeechView.swift
+//  Voicebus
+//
+//  Created by prispearls on 12/2/22.
+//
+
+import SwiftUI
+
+struct SpeechView: View {
+    var body: some View {
+        Text("siri pop up here")
+            .font(.title)
+    }
+}
+
+struct SpeechView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechView()
+    }
+}
